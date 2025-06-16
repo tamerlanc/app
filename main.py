f11856912eff8520a4e75b811229d176f66ffbd0
fet <alex09116rus@gmail.com>
2025-06-16 13:48:23 +0300

    Финальная рабочая конфигурация

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 
 class FinanceApp(App):
     def build(self):
-        return Button(text="Финансовое приложение", font_size=30)
+        layout = BoxLayout(orientation='vertical')
+        layout.add_widget(Label(text="Финансовое приложение", font_size=30))
+        layout.add_widget(Button(text="Начать работу", size_hint=(1, 0.5)))
+        return layout
 
 if __name__ == "__main__":
     FinanceApp().run()
