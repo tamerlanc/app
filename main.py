a35a210b35f138df0c8484a9f4d9b89e43236c3a
fet <alex09116rus@gmail.com>
2025-06-16 11:12:54 +0300

    test1

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+from kivy.app import App
+from kivy.uix.label import Label
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class FinanceApp(App):
+    def build(self):
+        return Label(text="Приложение для учета финансов")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    FinanceApp().run()
